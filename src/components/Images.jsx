a9b1c4be85bae1b923afaa8cc859b461d5a9b49c
Colin Bruin <colinjb23@gmail.com>
2018-07-08 15:33:50 -0400

    added the item id as a data attribute

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -6,27 +6,29 @@ import SingleImage from './SingleImage';
 export default class Images extends Component {
     constructor(props) {
         super(props);
-        this.state = {imageData: []}
+        this.state = { imageData: [] }
     }
 
     fetchImages = () => {
-        console.log('polling...');
         fetch("https://barter-mac.herokuapp.com/images")
             .then(res => res.json())
             .then(images => {
                 let willBeState = [];
-                
+
                 for (let i = 0; i < images.length; i++) {
                     let imgObject = {};
                     let imgItems = images[i].items;
-                        
-                    for (let j = 0; j < imgItems.length; j++){
-                        imgObject.userName = images[i].userName;
 
+                    for (let j = 0; j < imgItems.length; j++) {
+                        imgObject.userName = images[i].userName;
+                        console.log(imgItems[j]._id)
+
+                        let itemID = imgItems[j]._id;
                         let itemName = imgItems[j].itemName;
                         let imageUrl = imgItems[j].imageURL;
                         let imageDescription = imgItems[j].description;
 
+                        imgObject.itemID = itemID;
                         imgObject.itemName = itemName;
                         imgObject.imageURL = imageUrl;
                         imgObject.imageDescription = imageDescription;
@@ -35,25 +37,28 @@ export default class Images extends Component {
                         // We had to reset the imgObject object because the above code
                         // was replacing the old object even after it was pushed
                         imgObject = {};
-                    }  
+                    }
                 }
+                console.log(willBeState);
                 this.setState({
+                    // imageData should be changed to itemData
                     imageData: willBeState
                 })
             })
     }
     componentDidMount = () => {
         this.fetchImages();
-       const poll = setInterval(this.fetchImages, 5000);
+        const poll = setInterval(this.fetchImages, 5000);
         poll;
     }
-     
 
-    render() { 
-        const imgItems = this.state.imageData.map((image, i) => {        
-            return(<SingleImage key={i.toString()} userName={image.userName} src={image.imageURL} description={image.imageDescription}/>)
+
+    render() {
+        console.log(this.state.imageData);
+        const imgItems = this.state.imageData.map((image, i) => {
+            return (<SingleImage key={i.toString()} itemID={image.itemID} userName={image.userName} src={image.imageURL} description={image.imageDescription} />)
         });
-        return(
+        return (
             <React.Fragment>
                 <div>
                     {imgItems}
--- src/components/SingleImage.jsx
+++ src/components/SingleImage.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
 const SingleImage = (props) =>  { 
+    console.log(props.itemID);
     return(
-        <div>
+        <div data-item-id={props.itemID}>
             <img src={props.src} alt={props.src}></img>
             <p>posted by {props.userName}</p>
             <p>{props.description}</p>
